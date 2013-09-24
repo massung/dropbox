c893d710eb10b27563d75580957a2a19512afbce
Jeffrey Massung <massung@gmail.com>
2013-09-24 13:34:23 -0500

    Improved API calls. Updated README. Added dropbox-error condition. When an error occurs, parse the error and signal a condition.

--- README.md
+++ README.md
@@ -2,22 +2,30 @@
 
 The `dropbox` package is a wrapper for the [CORE Dropbox REST API](https://www.dropbox.com/developers/core/docs). It uses [OAuth 2.0](http://oauth.net/) for authentication and allows for manipulation of Dropbox data.
 
+This package requires the [`http`](https://www.github.com/massung/http) and [`json`](https://www.github.com/massung/json) packages (each has their own dependencies as well). Many of the Dropbox functions return RFC822 dates, which can be parsed into universal times with the [`date`](https://www.github.com/massung/date) package, but it isn't strictly required.
+
 ## Quickstart
 
-*NOTE: This tutorial assumes you have an application registered with Dropbox and that `*app-key*` and `*app-secret*` are set already.*
+There are special variables you'll need to set before you can use this tutorial: `*app-key*`, `*app-secret*`, and `*app-root*`.
+
+	(setq dropbox:*app-key* "myappkey")
+	(setq dropbox:*app-secret* "myappsecret")
+	(setq dropbox:*app-root* :sandbox)
+
+The variable `*app-root*` needs to be set to either `:dropbox` or `:sandbox`, based on the type of application you have (either full access or limited to just the `/Apps` folder). The default value is `:sandbox`.
 
 First, you'll need to request a token.
 
-	CL-USER > (dropbox-request-token *app-key*)
+	CL-USER > (dropbox-request-token)
 	#<HQN-WEB::INTERNET-EXPLORER HINSTANCE 42>
 
 This will open up a web browser window to Dropbox. If the user is already logged in it will prompt them to allow your application access. Otherwise it gives them the ability to log in first and then allow access.
 
 Once access has been granted, a token will be displayed on the webpage, which must then be used to complete the process.
 
-	CL-USER > (dropbox-request-access *app-key* *app-secret* token)
+	CL-USER > (dropbox-request-access token)
 	#<DROPBOX::ACCESS-TOKEN 200A5CFF>
-	
+
 Now that you have the token, you can use it to perform all sorts of fun Dropbox actions.
 
 	CL-USER > (dropbox-account-info *)
@@ -26,3 +34,7 @@ Now that you have the token, you can use it to perform all sorts of fun Dropbox 
 That's it!
 
 You can now download files, upload files, and more.
+
+## Basic Functionality
+
+TODO:
--- dropbox.lisp
+++ dropbox.lisp
@@ -20,23 +20,79 @@
 (defpackage :dropbox
   (:use :cl :http :json)
   (:export
+   #:*app-key*
+   #:*app-secret*
+   #:*app-root*
+
+   ;; Conditions
+   #:dropbox-error
+
    ;; OAuth functions
    #:dropbox-request-token
    #:dropbox-request-access
 
    ;; REST API end-points
    #:dropbox-account-info
-   #:dropbox-files
+   #:dropbox-files-get
+   #:dropbox-files-put
+   #:dropbox-media
    #:dropbox-metadata
    #:dropbox-revisions
+   #:dropbox-shares
+   #:dropbox-thumbnails
    
    ;; access token readers
    #:access-token
    #:access-token-uid
    #:access-token-type
-   ))
+
+   ;; account info readers
+   #:account-info-referral-link
+   #:account-info-display-name
+   #:account-info-uid
+   #:account-info-country
+   #:account-info-quota
+   #:account-info-email
+
+   ;; media readers
+   #:media-url
+   #:media-expires
+
+   ;; metadata readers
+   #:metadata-size
+   #:metadata-bytes
+   #:metadata-path
+   #:metadata-dir-p
+   #:metadata-deleted-p
+   #:metadata-rev
+   #:metadata-hash
+   #:metadata-thumb-p
+   #:metadata-icon
+   #:metadata-modified
+   #:metadata-client-mtime
+   #:metadata-root
+   #:metadata-mime-type
+   #:metadata-contents
+
+   ;; quota info readers
+   #:quota-datastores
+   #:quota-shared
+   #:quota
+   #:quota-normal))
 
 (in-package :dropbox)
+
+(define-condition dropbox-error (error)
+  ((|error| :reader dropbox-error))
+  (:report (lambda (c stream)
+             (format stream "Dropbox error: ~s" (dropbox-error c)))))
+
+(defvar *app-key* nil
+  "The App Key for your application. Provided by Dropbox.")
+(defvar *app-secret* nil
+  "The App Secret for your application. Provided by Dropbox.")
+(defvar *app-root* :sandbox
+  "Either :dropbox or :sandbox, based on the application's scope.")
 
 (defclass access-token ()
   ((|access_token| :reader access-token)
@@ -60,6 +116,11 @@
    (|email|         :reader account-info-email))
   (:documentation "Account information."))
 
+(defclass media ()
+  ((|url|     :reader media-url)
+   (|expires| :reader media-expires))
+  (:documentation "Media information."))
+
 (defclass metadata ()
   ((|size|         :reader metadata-size)
    (|bytes|        :reader metadata-bytes)
@@ -73,12 +134,17 @@
    (|modified|     :reader metadata-modified)
    (|client_mtime| :reader metadata-client-mtime)
    (|root|         :reader metadata-root)
+   (|mime_type|    :reader metadata-mime-type)
    (|contents|     :reader metadata-contents :type metadata))
   (:documentation "Metadata for any content file or directory."))
 
 (defclass revision (metadata)
   ()
   (:documentation "Revision metadata."))
+
+(defclass shares (media)
+  ()
+  (:documentation "Shares result."))
 
 (defconstant +auth-url+ "https://www.dropbox.com/1/oauth2/authorize"
   "End-point for requesting an authorization token.")
@@ -93,43 +159,72 @@
   "Create an authorization header for all REST API end-point requests."
   (list "Authorization" (format nil "~@(~a~) ~a" (access-token-type token) (access-token token))))
 
+(defun end-point (api &optional path)
+  "Create the path end-point for any given API call."
+  (format nil "/1/~a~:[~;/~(~a~)~a~]" api path *app-root* path))
+
 (defun parse-json-response (type resp)
   "Decode a JSON response into a type."
-  (when (= (response-code resp) 200)
-    (json-decode-into type (response-body resp) (request-url (response-request resp)))))
+  (let ((body (response-body resp))
+        (req (response-request resp)))
+    (if (= (response-code resp) 200)
+        (json-decode-into type body (request-url req))
+      (error (json-decode-into 'dropbox-error body (request-url req))))))
 
-(defun dropbox-request (type method url token)
+(defun dropbox-request (type method url token &key data)
   "Perform an HTTP request, and if successful, parse the JSON body."
-  (let ((req (make-instance 'request :url url :method method :headers (list (auth-header token)))))
+  (let ((req (make-instance 'request :data data :url url :method method :headers (list (auth-header token)))))
     (parse-json-response type (http-perform req))))
 
-(defun dropbox-request-token (key)
+(defun dropbox-request-token ()
   "Open the Dropbox authorization end-point to get a request token."
-  (with-url (url +auth-url+ :query `(("response_type" "code") ("client_id" ,key)))
+  (with-url (url +auth-url+ :query `(("response_type" "code") ("client_id" ,*app-key*)))
     (sys:open-url (format-url url))))
 
-(defun dropbox-request-access (key secret token)
+(defun dropbox-request-access (token)
   "Perform the POST request to obtain the access token."
   (let ((qs `(("grant_type" "authorization_code") ("code" ,token))))
-    (with-url (url +token-url+ :auth (list key secret) :query qs)
+    (with-url (url +token-url+ :auth (list *app-key* *app-secret*) :query qs)
       (parse-json-response 'access-token (http-post url)))))
 
 (defun dropbox-account-info (token)
   "Perform a GET to /account/info."
-  (with-url (url +api-url+ :path "/1/account/info")
+  (with-url (url +api-url+ :path (end-point "account/info"))
     (dropbox-request 'account-info "GET" url token)))
 
-(defun dropbox-files (token &key (root "dropbox") (path "/"))
-  "Perform a GET to /1/files/<root>/<path>."
-  (with-url (url +content-url+ :path (format nil "/1/files/~a~a" root path))
-    (http-get url :headers (list (auth-header token)))))
+(defun dropbox-files-get (token path)
+  "Perform a GET on a file."
+  (with-url (url +content-url+ :path (end-point "files" path))
+    (let ((resp (http-get url :headers (list (auth-header token)))))
+      (when (= (response-code resp) 200)
+        (response-body resp)))))
 
-(defun dropbox-metadata (token &key (root "dropbox") (path "/"))
+(defun dropbox-files-put (token path data)
+  "Uploads a file."
+  (with-url (url +content-url+ :path (end-point "files" path))
+    (dropbox-request 'metadata "PUT" url token :data data)))
+
+(defun dropbox-media (token path)
+  "Returns a direct link to a file."
+  (with-url (url +api-url+ :path (end-point "media" path))
+    (dropbox-request 'media "POST" url token)))
+
+(defun dropbox-metadata (token path)
   "Fetch the metadata for a give file or folder."
-  (with-url (url +api-url+ :path (format nil "/1/metadata/~a~a" root path))
+  (with-url (url +api-url+ :path (end-point "metadata" path))
     (dropbox-request 'metadata "GET" url token)))
 
-(defun dropbox-revisions (token &key (root "dropbox") (path "/"))
+(defun dropbox-revisions (token path)
   "Obtains metadata for the previous revisions of a file."
-  (with-url (url +api-url+ :path (format nil "/1/revisions/~a~a" root path))
+  (with-url (url +api-url+ :path (end-point "revisions" path))
     (dropbox-request 'revision "GET" url token)))
+
+(defun dropbox-shares (token path)
+  "Creates and returns a Dropbox link to files or folders."
+  (with-url (url +api-url+ :path (end-point "shares" path))
+    (dropbox-request 'shares "POST" url token)))
+
+(defun dropbox-thumbnails (token path &key (format "jpeg") (size "s"))
+  "Gets a thumbnail image."
+  (with-url (url +content-url+ :path (end-point "thumbnails" path) :query `(("format" ,format) ("size" ,size)))
+    (http-get url :headers (list (auth-header token)))))
